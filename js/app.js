73fdf3160a5441353a66bc8508294af1a37a52ca
airul-nasrul <18ftt1704@student.pb.edu.bn>
2020-04-13 17:42:27 +0800

    scope change 2

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/WADT-AS2/ayam", {scope: "./"})
+      .register("/WADT-AS2/sw.js", {scope: "./"})
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err));
   });
