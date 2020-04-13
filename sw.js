ccf7f2f4103d25c2313ef4536a7a24a2eccd71d4
airul-nasrul <18ftt1704@student.pb.edu.bn>
2020-04-14 00:15:47 +0800

    fix path in sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,18 @@
 const staticAS2 = "WADT-AS2";
 const assets = [
-  "/",
-  "/index.html",
-  "/aboutus.html",
-  "/airul.html",
-  "/baizura.html",
-  "/blog.html",
-  "/css/style.css",
-  "/img/quote1.jpg",
-  "/img/quote2.jpg",
-  "/img/quote3.jpg",
-  "/img/quote4.jpg",
-  "/img/quote5.jpg",
-  "/img/quote6.jpg"
+  "/WADT-AS2/",
+  "/WADT-AS2/index.html",
+  "/WADT-AS2/aboutus.html",
+  "/WADT-AS2/airul.html",
+  "/WADT-AS2/baizura.html",
+  "/WADT-AS2/blog.html",
+  "/WADT-AS2/css/style.css",
+  "/WADT-AS2/img/quote1.jpg",
+  "/WADT-AS2/img/quote2.jpg",
+  "/WADT-AS2/img/quote3.jpg",
+  "/WADT-AS2/img/quote4.jpg",
+  "/WADT-AS2/img/quote5.jpg",
+  "/WADT-AS2/img/quote6.jpg"
 ];
 
 self.addEventListener("install", installEvent => {
